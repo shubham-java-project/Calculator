32969c95b5b54ee904de1c30ddbd4a71760253c1
shubham <javadev.shubham1995@gmail.com>
2019-10-06 17:17:51 -0700

    adding Adding_Names.java

---
+++ Adding_Names/src/com/app/model/DisplayNames.java
@@ -0,0 +1,16 @@
+package com.app.model;
+
+public class DisplayNames {
+
+	public static void main(String[] args) {
+
+		String [] names= {"Ramesh","Rajesh","Ashok","Kumar"};
+		/**
+		 * printing names
+		 */
+		for(int i=0;i<=names.length-1;i++) {
+			System.out.println(names[i]);
+		}
+	}
+
+}
